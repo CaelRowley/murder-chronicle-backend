1436a41cd6566a5876e48711480698f42c6592e9
Cael Rowley <drdeus@pm.me>
2024-01-30 22:11:38 +0100

    chore: remove ts project

--- src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import Router from 'koa-router'
-import hello from 'routes/hello'
-import openai from 'routes/openai'
-
-const router = new Router()
-router.use('/hello', hello.routes())
-router.use('/openai', openai.routes())
-
-export default router
--- src/utils/services/logger.ts
+++
@@ -1,5 +0,0 @@
-import { createLogger } from 'bunyan'
-
-const logger = createLogger({name: "open-ai-backend"})
-
-export default logger
